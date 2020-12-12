38658714200302476c887a67c49de89a3018186f
Simon <simibac93@gmail.com>
2020-12-12 16:08:27 +0100

    Initial Commit

---
+++ Shared/SeatingSwfitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeatingSwfitUIApp.swift
+//  Shared
+//
+//  Created by Simon Bachmann on 12.12.20.
+//
+
+import SwiftUI
+
+@main
+struct SeatingSwfitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
